b819ba7a1ddf23bbdd5b3ab49fbcdca7cfdac779
Parvez141300 <parvezhossain744471@gmail.com>
2026-01-04 22:10:13 +0600

    created a seedAdmin function

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middleware/authMiddleware";
+
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: process.env.ADMIN_NAME,
+      email: process.env.ADMIN_EMAIL,
+      role: UserRole.ADMIN,
+      password: process.env.ADMIN_PASS
+    }
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email as string
+      }
+    })
+    if (existingUser) {
+      throw new Error("User already exists!!!");
+    }
+    const signUpAdmin = await fetch('http://localhost:3000/api/auth/sign-up/email', {
+      method: 'POST',
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(adminData)
+    });
+
+    console.log(signUpAdmin);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+seedAdmin();
