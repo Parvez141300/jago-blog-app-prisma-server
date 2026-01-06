48addc84123b77d49d9704e16d0326a0f6a36455
Parvez141300 <parvezhossain744471@gmail.com>
2026-01-06 18:30:37 +0600

    edited the comment service code

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -7,6 +7,18 @@ const createCommentIntoDB = async (payload: {
     parent_id?: string
 }) => {
     console.log(payload);
+    await prisma.post.findUniqueOrThrow({
+        where: {
+            id: payload.post_id
+        }
+    })
+    if(payload.parent_id){
+        await prisma.comment.findUniqueOrThrow({
+            where: {
+                id: payload.parent_id
+            }
+        })
+    }
     const result = await prisma.comment.create({
         data: payload
     });
